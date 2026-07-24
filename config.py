85a00ba4db583fbd232f1845e0ca74b47bfac796
youtgg0007-cell <youtgg0007@gmail.com>
2026-07-24 12:02:53 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-ADMIN_BOT_TOKEN = os.getenv("ADMIN_BOT_TOKEN", "8906347735:AAHiHksd28uRWd5YdsVTynYMv9vCz5rPuxg")
-STORE_BOT_TOKEN = os.getenv("STORE_BOT_TOKEN", "8546642077:AAGeBkvSJ9NS8D3C8bEg_xdIjwMXftYJ0bU")
+ADMIN_BOT_TOKEN = os.getenv("ADMIN_BOT_TOKEN", "8929358983:AAG8_6ZDZcjgnDWPPTd3UX-tawjJ8IZN8ao")
+STORE_BOT_TOKEN = os.getenv("STORE_BOT_TOKEN", "8987080415:AAGeICGQQlUCyctIsdQUxGLlwS93lLVrT4Q")
 
 # Owner(s) - can add/remove other sellers. Comma separated telegram user ids.
-OWNER_IDS = [int(x) for x in os.getenv("OWNER_IDS", "6594079594").split(",") if x.strip().isdigit()]
+OWNER_IDS = [int(x) for x in os.getenv("OWNER_IDS", "8656857571").split(",") if x.strip().isdigit()]
 
 STORE_NAME = "Uchiro Store 🇰🇭"
 CURRENCY = "$"
